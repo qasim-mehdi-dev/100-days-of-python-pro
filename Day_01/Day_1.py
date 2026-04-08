5a9c1c0666522b4af3bf088b5c3cf7089610190b
qasim-mehdi-dev <qasim.dev@proton.me>
2026-04-08 16:44:25 +0530

    Day 1 Band Name Generator

--- Day_01/Day_1.py
+++ Day_01/Day_1.py
@@ -1,1 +1,4 @@
-print("Hello World")
+print("Welcome to the Brand Name Generator")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet name?\n")
+print("Your band name could be: " + city + " " + pet)
