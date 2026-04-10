5c66870149a48900aa66674e45e6e9c9393cc2cc
Qasim Mehdi <qasim.dev@proton.me>
2026-04-10 19:44:41 +0530

    Day 3: Treasure Island complete with nested conditional logic

--- Day_02/Day_02.py
+++ Day_02/Day_02.py
@@ -1,11 +1,11 @@
-print("Welcome to the tip calculator!")
+print("Welcome to the Tip Calculator!")
 
 bill = float(input("What was the total bill? $"))
-tip = int(input("How much tip you would like to give? 10, 12 or 15 "))
-split = int(input("How many people to split the bill? "))
+tip = int(input("How much tip would you like to give? 10, 12 or 15 "))
+split = int(input("How much people you would like to split the bill with? "))
 
 total_bill = bill + (bill * tip / 100)
 each_person = total_bill / split
-final_bill = round(each_person, 2)
+final_amount = round(each_person, 2)
 
-print(f"Each person should pay: ${final_bill}")
+print(f"Each person should pay: {final_amount}")
