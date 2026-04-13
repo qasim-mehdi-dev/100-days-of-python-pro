ecbbeecf4629313ad7c7eadac5fc083f44801945
Qasim Mehdi <qasim.dev@proton.me>
2026-04-13 15:06:49 +0530

    Day 4: Rock Paper Scissors built from scratch - Mastered Lists and Randomization

--- Day_03/Day_03.py
+++ Day_03/Day_03.py
@@ -1,50 +1,41 @@
-print('''*******************************************************************************
-          |                   |                  |                     |
- _________|________________.=""_;=.______________|_____________________|_______
-|                   |  ,-"_,=""     '"=.|                  |
-|___________________|__"=._o'"-._        '"=.______________|___________________
-          |                '"=._o'"=._      _'"=._                     |
- _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
-|                   |    __.--" , ; '"=._o." ,-"""-._ ".   |
-|___________________|_._"  ,. .' ' '' ,  '"-._"-._   ". '__|___________________
-          |           |o'"=._' , "' '; .". ,  "-._"-._; ;              |
- _________|___________| ;'-.o'"=._; ." ' ''."' . "-._ /_______________|_______
-|                   | |o;    '"-.o'"=._''  '' " ,__.--o;   |
-|___________________|_| ;     (#) '-.o '"=.'_.--"_o.-; ;___|___________________
-____/______/______/___|o;._    "      '".o|o_.--"    ;o;____/______/______/____
-/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
-____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
-/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
-____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
-/______/______/______/______/______/______/______/______/______/______/_____ /
-*******************************************************************************
-''')
+print("Welcome to Rollercoaster")
+height = int(input("What is your height in cm? "))
+bill = 0
 
-print("Welcome to Treasure Island")
-print("Your mission is to find the treasure.")
-choice1 = input("You are at a crossroad. Where do you want to go?"
-                " Type 'left' or 'right'. ").lower()
-if choice1 == "right":
-    choice2 = input('You\'ve come to a lake. '
-                    'There is an Island in the middle of the lake. '
-                    'Type "wait" to wait for a boat.'
-                    'Type "swim" to swim across.').lower()
-    if choice2 == "swim":
-        choice3 = input("You arrived at the island unharmed. "
-                        "There is a house with three doors. "
-                        "One red, one yellow, one blue."
-                        " Which color do you choose? ").lower()
-        if choice3 == "yellow":
-            print("You enter a room full of beasts. Game Over.")
-        elif choice3 == "red":
-            print("It's a room full of fire. Game Over.")
-        elif choice3 == "blue":
-            print("You found the treasure. You Win!")
-        else:
-            print("You choose a door that doesn't exists. Game Over.")
-
+if height >= 120:
+    print("You can ride the rollercoaster")
+    age = int(input("What is your age? "))
+    if age < 13:
+        print("Children ticket costs 5$")
+        bill += 5
+    elif age <= 18:
+        print("Youth ticket costs 7$")
+        bill += 7
+    elif 45 <= age <= 55:
+        print("Don't worry everything is going to be fine, Your ticket is free")
+        bill += 0
     else:
-        print("Sorry you got eaten by a Lion. Game Over.")
-
+        print("Adult ticket costs 12$")
+        bill += 12
+    photo_both = input("Do you want to take a photo? (y/n): ")
+    if photo_both == "y":
+        bill += 3
+    print(f"Your final bill is: {bill}")
 else:
-    print("You fall into a hole. Game Over.")
+    print("Sorry, come back when you are taller.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
